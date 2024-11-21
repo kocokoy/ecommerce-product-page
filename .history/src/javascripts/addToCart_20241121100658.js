ded1c3cb37b88ae2cdb6be36f442eb9173ae22cf
kocokoy <vanneryckoconstantino@gmail.com>
2024-11-21 15:44:11 +0800

    developing responsive

---
+++ .history/src/javascripts/addToCart_20241121100658.js
@@ -0,0 +1,63 @@
+import { displayImage } from "./image.js";
+import { quantity } from "./infoQuantityInteract.js";
+
+
+export function createAddToCartButton(){
+  const content = document.querySelector('.content');
+  const button = document.createElement('button');
+  const div = document.createElement('div');
+  const img = displayImage("/images/icon-cart.svg");
+  div.classList.add('add-to-cart-button-container');
+  button.classList.add('add-to-cart-button');
+  button.textContent = 'Add to Cart';
+  button.appendChild(img);
+  div.appendChild(button);
+  content.appendChild(div);
+
+  addtoCartButtonClicked();
+}
+
+
+function addtoCartButtonClicked(){
+  const button = document.querySelector('.add-to-cart-button');
+  button.addEventListener('click', () => {
+    addtoCartCountUpdate(quantity);
+  })
+}
+
+function addtoCartCountUpdate(quantity){
+  const cartContent = document.querySelector('.cart-content');
+  const cart = document.querySelector('.item-quantity');
+  if(quantity === 0){
+    cart.textContent = '';
+    cartContent.innerHTML = 'Your cart is empty';
+    return;
+  } 
+  cart.textContent = quantity;
+  populateCart(cartContent,cart);
+}
+function populateCart(cartContent,cart){
+  const totalPrice = 125 * quantity;
+  const html = `<div class="inside-cart-item">
+  <img class="inside-cart-item-image" src="/images/image-product-1-thumbnail.jpg" alt="">
+  <div class="inside-cart-item-info">
+    <div class="inside-cart-item-info-title">Fall Limited Edition Sneakers</div>
+    <div class="inside-cart-item-info-price">
+      <div class="inside-cart-item-info-price-with-quantity">$125.00 x ${quantity}</div>
+      <div class="inside-cart-item-info-price-sum">$${totalPrice}.00</div>
+    </div>
+  </div>
+  <div class="inside-cart-item-button"><img src="/images/icon-delete.svg" alt=""></div>
+  </div>
+  <button>Checkout</button> `;
+  cartContent.innerHTML = html;
+  deleteButtonClicked(cartContent,cart);
+}
+
+function deleteButtonClicked(cartContent,cart){
+  const deleteButtonElement = document.querySelector('.inside-cart-item-button');
+  deleteButtonElement.addEventListener('click', () => {
+    cartContent.innerHTML = 'Your cart is empty';
+    cart.textContent = '';
+  })
+}
